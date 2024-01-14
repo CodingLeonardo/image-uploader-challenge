c893eb28c7a92721dcedb0c3709bc7d3ba9d2565
Leonardo Rivero <codingleonardo@gmail.com>
2024-01-13 22:24:39 -0400

    Add feature on the image upload server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import morgan from "morgan"
+import cors from "cors"
 import dotenv from "dotenv"
+import uploadImagesRoutes from "./routes/uploadImages.routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors())
+app.use(morgan("dev"))
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript Server");
 });
+
+app.use("/image", uploadImagesRoutes)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/uploadImages.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import upload from "../middleware/uploadImages.middleware"
+import { postImage, getImage } from "../controller/uploadImages.controller"
+
+const router = Router()
+
+router.post("/upload", upload.single("image"), postImage)
+router.get("/:id", getImage)
+
+
+export default router
