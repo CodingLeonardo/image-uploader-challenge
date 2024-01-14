9334590234b96b99f4ad4dd120a4b7b94da701c0
Leonardo Rivero <codingleonardo@gmail.com>
2024-01-14 13:21:16 -0400

    Implement file upload feature in frontend

--- client/src/components/FormImageUploader.tsx
+++ client/src/components/FormImageUploader.tsx
@@ -16,7 +16,7 @@ const FormImageUploader = ({ onSubmit, onChange, onDrop }: FormImageUploaderProp
       <DragAndDrop handleDragging={() => "hola"
       } handleDrop={onDrop} />
       <p>Or</p>
-      <input id="image" name="file" type="file" accept="image/png image/jpg image/jpeg image/svg .png,.jpg,.jpeg,.svg,.png" onChange={onChange} />
+      <input id="image" name="image" type="file" accept="image/png image/jpg image/jpeg image/svg .png,.jpg,.jpeg" onChange={onChange} />
       <label className="ImageUploader-button" htmlFor="image">Choose a file</label>
     </form>
   )
--- client/src/components/ImageUploader.tsx
+++ client/src/components/ImageUploader.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, DragEventHandler, FormEvent, useState } from "react"
+import axios from "axios";
 import FormImageUploader from "./FormImageUploader";
 import ImageUploaded from "./ImageUploaded";
 import "../styles/ImageUploader.css"
@@ -16,25 +17,44 @@ const ImageUploader = () => {
     event.preventDefault()
   }
 
+  const postImage = async (image) => {
+    const formData = new FormData()
+    formData.append("image", image)
+    axios.post("http://localhost:8000/image/upload", formData, {
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent;
+        let precentage = Math.floor((loaded * 100) / total);
+        console.log("options");
+        console.log(precentage);
+        if (precentage < 100) {
+          setProgress(true)
+        }
+        setUploaded(true)
+      },
+    }).then((request) => {
+      console.log(request);
+      setImage(`http://localhost:8000/image/${request.data.filename}`)
+    })
+    .catch((err) => {
+      setProgress(false)
+      setUploaded(false)
+    })
+  }
+
   const handleChange = (event: FileEvent) => {
     const file = event.target.files[0]
-
     if(!event.target.files) {
       console.log("No hay Files!");
     }
-
-    setImage(URL.createObjectURL(file))
-    setProgress(true)
+    postImage(file)
   }
 
   const handleDrop = (event) => {
     const file = event.dataTransfer.files[0]
-
     if(!event.dataTransfer.files) {
       console.log("No hay Files!");
     }
-    setImage(URL.createObjectURL(file))
-    setProgress(true)
+    postImage(file)
   }
 
   if(progress) {
