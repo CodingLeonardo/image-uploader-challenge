cc2f912c839dd736c3177fb860bca7f897be475d
Leonardo Rivero <codingleonardo@gmail.com>
2024-04-12 21:18:14 -0400

    Add Dark Mode with CSS Custom Properties

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,42 @@
-import Logo from "../assets/logo.svg";
-import Moon from "../assets/Moon_fill.svg";
+import { useState, useEffect } from "react";
+import LogoIcon from "./LogoIcon";
 
 import "../styles/Header.css";
+import SunIcon from "./SunIcon";
+import MoonIcon from "./MoonIcon";
 
 const Header = () => {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html")!.classList.add("dark");
+    } else {
+      document.querySelector("html")!.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
   return (
     <header className="Header">
       <div className="container">
         <figure className="Header-logo">
-          <img src={Logo} alt="Logo" />
+          <LogoIcon />
         </figure>
-        <button className="Header-button">
-          <img src={Moon} alt="Moon" />
+        <button
+          className="Header-switch__theme"
+          aria-label="Change to light theme"
+          onClick={handleChangeTheme}
+        >
+          {theme === "light" ? <MoonIcon /> : <SunIcon />}
         </button>
       </div>
     </header>
